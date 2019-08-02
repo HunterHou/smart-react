a96dbc1ab273a156d403c6a82303820c0e498926
hunter <183598821@qq.com>
2019-08-02 14:58:18 +0800

    hd

--- src/component/RemarkList.js
+++ src/component/RemarkList.js
@@ -1,65 +1,71 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import RemarkItem from './RemarkItem'
+import '../css/RemarkList.css'
 
 class RemarkList extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      remarks: [],
-      inputValue: ''
-    };
-    this.inputChange = this.inputChange.bind(this);
-    this.addRemark = this.addRemark.bind(this);
+    constructor(props) {
+        super(props);
+        this.state = {
+            remarks: [],
+            inputValue: '',
+            sysTime:new Date().toLocaleTimeString()
+        };
+        this.inputChange = this.inputChange.bind(this);
+        this.addRemark = this.addRemark.bind(this);
 
-  }
-  inputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
+    }
 
-  }
-  addRemark() {
-    var { inputValue } = this.state;
-    this.setState({
-      remarks: [...this.state.remarks, inputValue]
-    })
-  }
+    inputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
 
-  removeRemark(index) {
-    var list = this.state.remarks;
-    list.splice(index, 1)
-    this.setState({ remarks: list })
-  }
+    }
 
-  render() {
-    return (
-      <div>
-        <div>
-          <input value={this.state.inputValue} onChange={this.inputChange} />
-          <button onClick={this.addRemark}>添加</button>
-        </div>
-        <div>
-          <table >
-            <tbody >
-              <tr>
-                <td>
-                  <ul>
-                    {this.state.remarks.map((item, index) => {
-                      return <RemarkItem key={index} index={index} item={item} removeRemark={this.removeRemark.bind(this)}></RemarkItem>
-                    })}
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
+    addRemark() {
+        var {inputValue} = this.state;
+        this.setState({
+            remarks: [...this.state.remarks, inputValue]
+        })
+    }
 
-          </table>
+    removeRemark(index) {
+        var list = this.state.remarks;
+        list.splice(index, 1)
+        this.setState({remarks: list})
+    }
 
-        </div>
-      </div>
+    render() {
+        return (
+            <div>
+                <div>
+                    <input value={this.state.inputValue} onChange={this.inputChange}/>
+                    <button onClick={this.addRemark}>添加</button>
+                    <span>{new Date().toLocaleTimeString()}</span>
+                </div>
+                <div>
+                    <table className="remark-list-table">
+                        <tbody>
+                        <tr>
+                            <td>
+                                <ul>
+                                    {this.state.remarks.map((item, index) => {
+                                        return <RemarkItem key={index} index={index} item={item}
+                                                           removeRemark={this.removeRemark.bind(this)}></RemarkItem>
+                                    })}
+                                </ul>
+                            </td>
+                        </tr>
+                        </tbody>
 
-    );
-  }
+                    </table>
+
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default RemarkList;
